23713381f48bd67770156b5a0f7cd9d008d83e8e
yanivabir <yanivabir@gmail.com>
2018-06-22 13:07:30 +0300

    testing

--- Code/static/js/task.js
+++ Code/static/js/task.js
@@ -620,7 +620,7 @@ var test_animation = {
   stimulus_alpha: stimAlphas,
   timing_post_trial: 100,
   within_ITI: ITI - 100,
-  timing_response: 4,
+  timing_response: 0,
   fade_in_time: fade_in_time,
   fade_out_time: fade_out_time,
   fade_out_length: fade_out_length,
